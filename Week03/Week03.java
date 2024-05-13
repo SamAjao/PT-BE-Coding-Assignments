d3b6b62f08d7e5a28313b6977cf97d3df4b708c8
unknown <samuel.ajao@gmail.com>
2024-05-13 16:11:12 -0700

    Added week 4 Coding Assignment to repo

--- Week03/Week03.java
+++ Week03/Week03.java
@@ -4,7 +4,19 @@ public class Week03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(power(4,2));
 
+	}
+	
+	public static int power(int base, int exponent) {
+		int result = 1;
+		
+		for(int i = 1; i<= exponent; i++) {
+			result *= base;
+		}
+		
+		return result;
 	}
 
 }
---
+++ Week04/AjaoJava4CodingAssignment.java
@@ -0,0 +1,15 @@
+/* Author: Samuel Ajao
+ * Course: Promineo Tech Back End Software Development Boot Camp
+ * Assignment: Java 4 Coding Project
+ * Date: May 13, 2024
+ */
+package Week04;
+
+public class AjaoJava4CodingAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
