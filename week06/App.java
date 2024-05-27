abc8ef39c513a07219f0c22040c0f831bff8cc46
unknown <samuel.ajao@gmail.com>
2024-05-26 22:03:02 -0700

    replaced hardcoded player names in Main with variables. Other cleanup.

--- week06/App.java
+++ week06/App.java
@@ -24,15 +24,21 @@ e.    Print the final score of each player and either “Player 1”, “Player 
 package week06;
 
 public class App {
+	
 
 	public static void main(String[] args) {
 		
 		//a. Instantiate a Deck and two Players, call the shuffle method on the deck.
 		Deck warDeck = new Deck("War Deck");
-		Player player1 = new Player("Player 1");
-		Player player2 = new Player("Player 2");
+		Player player1 = new Player("Michelle");
+		Player player2 = new Player("Tina");
 		warDeck.shuffleDeck();
-		System.out.println("Number of cards in Deck is: " + warDeck.getDeckSize());
+		System.out.println("Number of cards in Deck: " + warDeck.getDeckSize());
+		
+		System.out.println("--------------------------------------------------------------------------------------");
+		System.out.println("WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR WAR");
+		System.out.println("--------------------------------------------------------------------------------------");
+		
 		
 		//b. Using a traditional for loop, iterate 52 times calling the Draw method on
 		//		the other player each iteration using the Deck you instantiated.
@@ -47,20 +53,22 @@ public class App {
 		 * Note:  If the values are equal (it is a tie), print a message saying that no point was awarded.
 		 */
 		for(int i = 0; i < 26; i++) {
-			System.out.print("Player 1 Drew::: ");
+			System.out.println("Draw number: " + (i+1));
+			
+			System.out.print(player1.getName() + " Draws ::: ");
 			player1.topCardIs().describe();
-			System.out.print("Player 2 Drew::: ");
+			System.out.print(player2.getName() + " Draws ::: ");
 			player2.topCardIs().describe();
 			
 			
 			int result = player1.flip().getValue() - player2.flip().getValue();
 			if(result > 0) {
 				player1.incrementScore();
-				System.out.println("Point to Player 1!");
+				System.out.println("Point to " + player1.getName() + "!");
 			}
 			else if(result < 0) {
 				player2.incrementScore();
-				System.out.println("Point to Player 2!");
+				System.out.println("Point to " + player2.getName() + "!");
 			}
 			else {
 				System.out.println("Tie. No point awarded.");
@@ -74,16 +82,19 @@ public class App {
 		
 		//e. Print the final score of each player and either “Player 1”, “Player 2”, or “Draw” depending
 		//		on which score is higher or if they are both the same.
-		System.out.println("Player 1 Score: " + player1.getScore() + "\t---Player 2 Score: " + player2.getScore());
+		System.out.println("!!! FINAL SCORE !!!");
+		System.out.println(player1.getName() + "'s Score: " + player1.getScore() + "\t" + "---" + player2.getName() + "'s Score: " + player2.getScore());
 		if(finalScore > 0) {
-			System.out.println("Player 1 Wins!");
+			System.out.println(player1.getName() +" Wins!");
 		}
 		else if(finalScore < 0) {
-			System.out.println("Player 2 Wins!");
+			System.out.println(player2.getName() + " Wins!");
 		}
 		else {
-			System.out.println("Draw!");
+			System.out.println("Game is a Draw!");
 		}
+		
+		System.out.println("Number of cards in Deck: " + warDeck.getDeckSize());
 
 
 	}//End Main
